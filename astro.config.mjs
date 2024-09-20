3a3185834776e9e22a7dd3d489f4b7984d4c842c
Mijail Jhonatan Palomino Rodriguez <64047047+MP3003K@users.noreply.github.com>
2024-09-19 23:52:50 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  outDir: 'dist',  // Asegúrate de que los archivos generados se colocan en el directorio 'dist'
+  // Puedes agregar más configuraciones aquí si las necesitas
+});
